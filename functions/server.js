5fe58710ca0f5c5177b2236f78d19d37b0b685e6
Bilal Iqbal <bi.bilaliqbal@gmail.com>
2024-08-28 19:22:58 -0700

    cors

--- functions/server.js
+++ functions/server.js
@@ -10,13 +10,8 @@ require('../_seeder');
 
 
 app.use(bodyParser.json())
-<<<<<<< HEAD
-app.use(cors("http://localhost:3001"));
-// app.use(cors("https://userroles.netlify.app/"));
-=======
 // app.use(cors("http://localhost:3001"));
 app.use(cors("https://userroles.netlify.app"));
->>>>>>> 049b59af67c42342a975308188c85eb04baae824
 
 
 
